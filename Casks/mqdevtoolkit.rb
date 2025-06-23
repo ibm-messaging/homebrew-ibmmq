97d70c3ff14c17e18bdf11675c3022fe97d2cdfe
Soheel Chughtai <soheel_chughtai@uk.ibm.com>
2025-06-23 12:28:25 +0100

    Add OS architecture check

--- Casks/mqdevtoolkit.rb
+++ Casks/mqdevtoolkit.rb
@@ -144,6 +144,7 @@ cask "mqdevtoolkit" do
       puts "Checking machine architecture"
       osArch = `uname -m`
       puts osArch
+      puts type(osArch)
       if "arm64" != osArch
         puts bold("NOTE: Support for x86_64 MacOS will be dropped in the next release.")
       else
